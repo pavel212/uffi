dd9f219ea68b04c5daaf44ceff096993cc53f3ce
pv <pavel212@mail.ru>
2023-12-01 19:33:28 +0100

    fixed tcc build

--- build_tcc.bat
+++ build_tcc.bat
@@ -1,3 +1,3 @@
-tcc -m64 -shared -I"lib" src\*.c lib\lua54.def src\call_win64.S -o ffi.dll
+tcc -m64 -shared -I"lib" src\call_win64.S src\*.c lib\lua54.def -o ffi.dll
 
 @rem upx --ultra-brute ffi.dll
