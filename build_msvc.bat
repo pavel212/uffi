7cafa52fc3a4aec5e0c82e219df0a6ddaa820494
pv <pavel212@mail.ru>
2024-01-05 18:46:10 +0100

    @#$

--- build_msvc.bat
+++ build_msvc.bat
@@ -1,4 +1,4 @@
-cl src\*.c /c /GS- /sdl- /EHs-c- /GL /Oi /I"lib" /Zc:inline /Zc:forScope /fp:fast /O1 /MD
+cl src\ffi.c src\userdata.c src\win64.c /c /GS- /sdl- /EHs-c- /GL /Oi /I"lib" /Zc:inline /Zc:forScope /fp:fast /O1 /MD
 ml64 /c src\call_win64.asm                              
 link *.obj "lib\*.lib" "kernel32.lib" /OUT:"ffi.dll" /MANIFEST:NO /LTCG /NXCOMPAT /DYNAMICBASE /DLL /MACHINE:X64 /OPT:REF /INCREMENTAL:NO /SUBSYSTEM:WINDOWS /OPT:ICF /NOLOGO 
 @rem /NODEFAULTLIB
--- build_tcc.bat
+++ build_tcc.bat
@@ -1,3 +1,3 @@
-tcc -m64 -shared -I"lib" src\call_win64.S src\*.c lib\lua54.def -o ffi.dll
+tcc -m64 -shared -I"lib" -D_WIN64 src\call_win64.S src\ffi.c src\userdata.c src\win64.c lib\lua54.def -o ffi.dll
 @rem  
 @rem upx --ultra-brute ffi.dll
